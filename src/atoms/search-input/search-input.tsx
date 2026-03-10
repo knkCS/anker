dd7a311fc64758e7da287b79786bec312397025e
Jesko Iwanovski <jiwanovski@knk.de>
2026-03-10 14:31:31 +0100

    refactor(atoms): SearchInput re-exports forms version (consolidate)

--- src/atoms/search-input/search-input.tsx
+++ src/atoms/search-input/search-input.tsx
@@ -1,39 +1,5 @@
-import { Input, InputGroup, type InputProps } from "@chakra-ui/react";
-import debounce from "lodash.debounce";
-import { Search } from "lucide-react";
-import type React from "react";
-import { useRef } from "react";
-
-export interface SearchInputProps
-	extends Omit<InputProps, "onChange" | "defaultValue"> {
-	onChange: (term: string) => void;
-	maxWidth?: string;
-}
-
-export const SearchInput: React.FC<SearchInputProps> = (props) => {
-	const { value, onChange, maxWidth = "full", ...restProps } = props;
-
-	const debouncedSearch = useRef(
-		debounce((term: string) => {
-			onChange(term);
-		}, 300),
-	);
-
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		debouncedSearch.current(e.target.value);
-	};
-
-	return (
-		<InputGroup maxWidth={maxWidth} startElement={<Search size={16} />}>
-			<Input
-				bgColor="bg-subtle"
-				variant="outline"
-				type="text"
-				autoComplete="off"
-				defaultValue={String(value || "")}
-				onChange={handleChange}
-				{...restProps}
-			/>
-		</InputGroup>
-	);
-};
+/**
+ * Re-exported from forms/search-input for backwards compatibility.
+ * The forms version has more configuration options (debounceMs, placeholder).
+ */
+export { SearchInput, type SearchInputProps } from "../../forms/search-input";
