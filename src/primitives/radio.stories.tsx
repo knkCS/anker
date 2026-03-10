4db5ca0ccc5750964801a8acf1dcc258e6dacbc7
Jesko Iwanovski <jiwanovski@knk.de>
2026-03-10 11:34:27 +0100

    feat(primitives): add Radio

---
+++ src/primitives/radio.stories.tsx
@@ -0,0 +1,52 @@
+import { Stack } from "@chakra-ui/react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { Radio, RadioGroup } from "./radio";
+
+const meta = {
+	title: "Primitives/Radio",
+	component: Radio,
+} satisfies Meta<typeof Radio>;
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {
+	render() {
+		return (
+			<RadioGroup defaultValue="option1">
+				<Stack gap="2">
+					<Radio value="option1">Option 1</Radio>
+					<Radio value="option2">Option 2</Radio>
+					<Radio value="option3">Option 3</Radio>
+				</Stack>
+			</RadioGroup>
+		);
+	},
+};
+
+export const Sizes: Story = {
+	render() {
+		return (
+			<Stack gap="4">
+				<RadioGroup defaultValue="sm" size="sm">
+					<Stack direction="row" gap="4">
+						<Radio value="sm">Small</Radio>
+						<Radio value="sm2">Small 2</Radio>
+					</Stack>
+				</RadioGroup>
+				<RadioGroup defaultValue="md" size="md">
+					<Stack direction="row" gap="4">
+						<Radio value="md">Medium</Radio>
+						<Radio value="md2">Medium 2</Radio>
+					</Stack>
+				</RadioGroup>
+				<RadioGroup defaultValue="lg" size="lg">
+					<Stack direction="row" gap="4">
+						<Radio value="lg">Large</Radio>
+						<Radio value="lg2">Large 2</Radio>
+					</Stack>
+				</RadioGroup>
+			</Stack>
+		);
+	},
+};
---
+++ src/primitives/radio.tsx
@@ -0,0 +1,25 @@
+import { RadioGroup as ChakraRadioGroup } from "@chakra-ui/react";
+import type * as React from "react";
+
+export interface RadioProps extends ChakraRadioGroup.ItemProps {
+	rootRef?: React.RefObject<HTMLDivElement | null>;
+	inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
+}
+
+export const Radio = function Radio({
+	ref,
+	...props
+}: RadioProps & { ref?: React.Ref<HTMLInputElement> }) {
+	const { children, inputProps, rootRef, ...rest } = props;
+	return (
+		<ChakraRadioGroup.Item ref={rootRef} {...rest}>
+			<ChakraRadioGroup.ItemHiddenInput ref={ref} {...inputProps} />
+			<ChakraRadioGroup.ItemIndicator />
+			{children && (
+				<ChakraRadioGroup.ItemText>{children}</ChakraRadioGroup.ItemText>
+			)}
+		</ChakraRadioGroup.Item>
+	);
+};
+
+export const RadioGroup = ChakraRadioGroup.Root;
