8ce824cc4f3bf5c27403ed65551184e47ea8458f
Jesko Iwanovski <jiwanovski@knk.de>
2026-03-10 11:38:24 +0100

    feat(primitives): add Stat

---
+++ src/primitives/stat.stories.tsx
@@ -0,0 +1,65 @@
+import { Stack } from "@chakra-ui/react";
+import type { Meta, StoryObj } from "@storybook/react";
+import {
+	StatDownTrend,
+	StatHelpText,
+	StatLabel,
+	StatRoot,
+	StatUpTrend,
+	StatValueText,
+} from "./stat";
+
+const meta = {
+	title: "Primitives/Stat",
+	component: StatRoot,
+} satisfies Meta<typeof StatRoot>;
+
+export default meta;
+type Story = StoryObj;
+
+export const Default: Story = {
+	render() {
+		return (
+			<StatRoot>
+				<StatLabel>Total Revenue</StatLabel>
+				<StatValueText value={45670} formatOptions={{ style: "currency", currency: "EUR" }} />
+			</StatRoot>
+		);
+	},
+};
+
+export const WithTrend: Story = {
+	render() {
+		return (
+			<Stack direction="row" gap="8">
+				<StatRoot>
+					<StatLabel>Revenue</StatLabel>
+					<StatValueText value={345670} formatOptions={{ style: "currency", currency: "EUR" }} />
+					<StatHelpText>
+						<StatUpTrend>23.5%</StatUpTrend>
+					</StatHelpText>
+				</StatRoot>
+				<StatRoot>
+					<StatLabel>Costs</StatLabel>
+					<StatValueText value={120000} formatOptions={{ style: "currency", currency: "EUR" }} />
+					<StatHelpText>
+						<StatDownTrend>12.1%</StatDownTrend>
+					</StatHelpText>
+				</StatRoot>
+			</Stack>
+		);
+	},
+};
+
+export const WithInfoTip: Story = {
+	render() {
+		return (
+			<StatRoot>
+				<StatLabel info="Revenue from all sources this month">
+					Monthly Revenue
+				</StatLabel>
+				<StatValueText value={98500} formatOptions={{ style: "currency", currency: "EUR" }} />
+			</StatRoot>
+		);
+	},
+};
---
+++ src/primitives/stat.tsx
@@ -0,0 +1,71 @@
+import {
+	Badge,
+	type BadgeProps,
+	Stat as ChakraStat,
+	FormatNumber,
+} from "@chakra-ui/react";
+import type * as React from "react";
+import { InfoTip } from "./toggle-tip";
+
+interface StatLabelProps extends ChakraStat.LabelProps {
+	info?: React.ReactNode;
+}
+
+export const StatLabel = function StatLabel({
+	ref,
+	...props
+}: StatLabelProps & { ref?: React.Ref<HTMLDivElement> }) {
+	const { info, children, ...rest } = props;
+	return (
+		<ChakraStat.Label {...rest} ref={ref}>
+			{children}
+			{info && <InfoTip>{info}</InfoTip>}
+		</ChakraStat.Label>
+	);
+};
+
+interface StatValueTextProps extends ChakraStat.ValueTextProps {
+	value?: number;
+	formatOptions?: Intl.NumberFormatOptions;
+}
+
+export const StatValueText = function StatValueText({
+	ref,
+	...props
+}: StatValueTextProps & { ref?: React.Ref<HTMLDivElement> }) {
+	const { value, formatOptions, children, ...rest } = props;
+	return (
+		<ChakraStat.ValueText {...rest} ref={ref}>
+			{children ||
+				(value != null && <FormatNumber value={value} {...formatOptions} />)}
+		</ChakraStat.ValueText>
+	);
+};
+
+export const StatUpTrend = function StatUpTrend({
+	ref,
+	...props
+}: BadgeProps & { ref?: React.Ref<HTMLDivElement> }) {
+	return (
+		<Badge colorPalette="green" gap="0" {...props} ref={ref}>
+			<ChakraStat.UpIndicator />
+			{props.children}
+		</Badge>
+	);
+};
+
+export const StatDownTrend = function StatDownTrend({
+	ref,
+	...props
+}: BadgeProps & { ref?: React.Ref<HTMLDivElement> }) {
+	return (
+		<Badge colorPalette="red" gap="0" {...props} ref={ref}>
+			<ChakraStat.DownIndicator />
+			{props.children}
+		</Badge>
+	);
+};
+
+export const StatRoot = ChakraStat.Root;
+export const StatHelpText = ChakraStat.HelpText;
+export const StatValueUnit = ChakraStat.ValueUnit;
