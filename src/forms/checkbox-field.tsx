240ad3071481ac1565ff621a3e2a66582ab64184
Jesko Iwanovski <jiwanovski@knk.de>
2026-05-28 17:38:24 +0200

    feat(forms): dirty marker propagation on SwitchField + CheckboxField

--- src/forms/checkbox-field.tsx
+++ src/forms/checkbox-field.tsx
@@ -1,34 +1,41 @@
-import { Checkbox, Field } from "@chakra-ui/react";
+import { Checkbox } from "@chakra-ui/react";
 import type React from "react";
-import {
-	Controller,
-	type FieldValues,
-	type Path,
-	useFormContext,
-} from "react-hook-form";
+import type { FieldValues, Path } from "react-hook-form";
+import type { FormFieldProps } from "./form-field";
+import { FormField } from "./form-field";
 
-export interface CheckboxFieldProps<T extends FieldValues> {
-	name: Path<T>;
-	label?: string;
+export interface CheckboxFieldProps<T extends FieldValues>
+	extends Omit<FormFieldProps<T>, "children"> {
 	/** When provided, the field is treated as an array of checked values. */
 	value?: string | number;
-	disabled?: boolean;
-	helperText?: React.ReactNode;
+	/** Content to display after the checkbox. */
 	children?: React.ReactNode;
 }
 
 export function CheckboxField<T extends FieldValues>({
-	ref,
 	...props
-}: CheckboxFieldProps<T> & { ref?: React.Ref<HTMLLabelElement> }) {
-	const { name, label, value, disabled, helperText, children } = props;
-	const { control } = useFormContext<T>();
+}: CheckboxFieldProps<T>) {
+	const {
+		name,
+		label,
+		value,
+		disabled,
+		showDirtyState,
+		helperText,
+		children,
+		...rest
+	} = props;
 
 	return (
-		<Controller
+		<FormField<T>
 			name={name}
-			control={control}
-			render={({ field, fieldState }) => {
+			label={label}
+			disabled={disabled}
+			showDirtyState={showDirtyState}
+			helperText={helperText}
+			{...rest}
+		>
+			{(field) => {
 				// Array mode: field.value is an array, value prop identifies the item
 				const isArrayMode = value !== undefined;
 
@@ -60,39 +67,29 @@ export function CheckboxField<T extends FieldValues>({
 				const uniqueId = isArrayMode ? `${name}-${String(value)}` : name;
 
 				return (
-					<Field.Root invalid={!!fieldState.error} disabled={disabled}>
-						<Checkbox.Root
-							id={uniqueId}
-							name={field.name}
-							value={isArrayMode ? String(value) : undefined}
-							invalid={!!fieldState.error}
-							ref={ref}
-							checked={isChecked}
-							onCheckedChange={handleCheckedChange}
-							onBlur={field.onBlur}
-							disabled={disabled}
-						>
-							<Checkbox.HiddenInput />
-							<Checkbox.Control>
-								<Checkbox.Indicator />
-							</Checkbox.Control>
-							{(label || children) && (
-								<Checkbox.Label>
-									{label}
-									{children}
-								</Checkbox.Label>
-							)}
-						</Checkbox.Root>
-						{helperText && <Field.HelperText>{helperText}</Field.HelperText>}
-						{fieldState.error && (
-							<Field.ErrorText aria-live="polite">
-								{fieldState.error.message}
-							</Field.ErrorText>
+					<Checkbox.Root
+						id={uniqueId}
+						name={field.name}
+						value={isArrayMode ? String(value) : undefined}
+						checked={isChecked}
+						onCheckedChange={handleCheckedChange}
+						onBlur={field.onBlur}
+						disabled={disabled}
+						aria-describedby={field["aria-describedby"]}
+					>
+						<Checkbox.HiddenInput />
+						<Checkbox.Control>
+							<Checkbox.Indicator />
+						</Checkbox.Control>
+						{(label || children) && (
+							<Checkbox.Label>
+								{children}
+							</Checkbox.Label>
 						)}
-					</Field.Root>
+					</Checkbox.Root>
 				);
 			}}
-		/>
+		</FormField>
 	);
 }
 (CheckboxField as { displayName?: string }).displayName = "CheckboxField";
