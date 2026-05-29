da0763896f64442e730c8b24e6efa0926e3c207c
Jesko Iwanovski <jiwanovski@knk.de>
2026-05-29 15:35:00 +0200

    feat(navigation): wire navigation sub-path export

---
+++ src/navigation/index.ts
@@ -0,0 +1,18 @@
+// useUnsavedChangesBlocker
+export {
+	useUnsavedChangesBlocker,
+	type UnsavedChangesBlockerOptions,
+} from "./use-unsaved-changes-blocker";
+
+// UnsavedChangesGuard
+export {
+	UnsavedChangesGuard,
+	type UnsavedChangesGuardProps,
+} from "./unsaved-changes-guard";
+
+// TabDirtyContext
+export {
+	TabDirtyProvider,
+	useTabDirty,
+	type TabDirtyState,
+} from "./tab-dirty-context";
